70cce0410d0faccf8bb260172fcebf4e2f9e206c
Paul Collins <paul.collins@canonical.com>
2019-01-16 16:39:42 +1300

    test__get_account_usage: remove ADMIN_SWIFT_ACCOUNT and open-code the value
    I could not decide whether this made sense, or why I would
    stop at this and not parameterize, say, the partition number,
    or other things besides, as well.

--- tests/test_SwiftAccountUsage.py
+++ tests/test_SwiftAccountUsage.py
@@ -7,9 +7,6 @@ from requests.structures import CaseInsensitiveDict
 import prometheus_openstack_exporter as poe
 
 
-ADMIN_SWIFT_ACCOUNT = 'AUTH_12bb569bf909441b90791482ae6f9ca9'
-
-
 class TestSwiftAccountUsage(unittest.TestCase):
 
     @patch('prometheus_openstack_exporter.SwiftAccountUsage._get_account_ring')
@@ -38,13 +35,13 @@ class TestSwiftAccountUsage(unittest.TestCase):
         poe.requests.head.return_value = response_mock
 
         # Assert that _get_account_ring does what we expect.
-        self.assertEqual(s._get_account_usage(ADMIN_SWIFT_ACCOUNT), 368259416)
+        self.assertEqual(s._get_account_usage('AUTH_12bb569bf909441b90791482ae6f9ca9'), 368259416)
 
         # Assert that _get_account_ring did it in the manner we expected.
-        s.account_ring.get_nodes.assert_called_once_with(account=ADMIN_SWIFT_ACCOUNT)
+        s.account_ring.get_nodes.assert_called_once_with(account='AUTH_12bb569bf909441b90791482ae6f9ca9')
         poe.requests.head.assert_called_once()
         self.assertTrue(poe.requests.head.call_args in [
-            call('http://10.24.0.18:6002/sdb/26701/{}'.format(ADMIN_SWIFT_ACCOUNT)),
-            call('http://10.24.0.71:6002/sdd/26701/{}'.format(ADMIN_SWIFT_ACCOUNT)),
-            call('http://10.24.0.72:6002/sdi/26701/{}'.format(ADMIN_SWIFT_ACCOUNT)),
+            call('http://10.24.0.18:6002/sdb/26701/AUTH_12bb569bf909441b90791482ae6f9ca9'),
+            call('http://10.24.0.71:6002/sdd/26701/AUTH_12bb569bf909441b90791482ae6f9ca9'),
+            call('http://10.24.0.72:6002/sdi/26701/AUTH_12bb569bf909441b90791482ae6f9ca9'),
             ])
