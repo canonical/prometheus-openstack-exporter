1f7553ba5061d31c9599dc9eb068a6be706a2e5c
Paul Collins <paul.collins@canonical.com>
2019-01-24 08:57:47 +1300

    test__get_account_usage: mock smarter, not harder
    @patch already creates MagicMocks, so stop duplicating that work.
    
    Patching _get_account_ring means that s.account_ring will be a MagicMock already.
    
    And patch requests.head only in prometheus_openstack_exporter.

--- tests/test_SwiftAccountUsage.py
+++ tests/test_SwiftAccountUsage.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mock import Mock, MagicMock, call, patch
+from mock import Mock, call, patch
 
 from requests.structures import CaseInsensitiveDict
 
@@ -10,29 +10,25 @@ import prometheus_openstack_exporter as poe
 class TestSwiftAccountUsage(unittest.TestCase):
 
     @patch('prometheus_openstack_exporter.SwiftAccountUsage._get_account_ring')
-    @patch('prometheus_openstack_exporter.requests')
+    @patch('prometheus_openstack_exporter.requests.head')
     @patch('prometheus_openstack_exporter.config')
-    def test__get_account_usage(self, _config, _requests, _SwiftAccountUsage__get_account_ring):
-        _config = MagicMock()  # noqa: F841
-        _requests = MagicMock()  # noqa: F841
-        _SwiftAccountUsage__get_account_ring = MagicMock()  # noqa: F841
-
+    def test__get_account_usage(self, _config, _requests_head, _SwiftAccountUsage__get_account_ring):
         s = poe.SwiftAccountUsage()
-        s.account_ring = MagicMock()
         s.account_ring.get_nodes.return_value = (26701, [
             {'device': 'sdb', 'id': 0, 'ip': '10.24.0.18', 'meta': u'', 'port': 6002,
              'region': 1, 'replication_ip': '10.24.0.18', 'replication_port': 6002, 'weight': 100.0, 'zone': 1},
             {'device': 'sdd', 'id': 50, 'ip': '10.24.0.71', 'meta': u'', 'port': 6002,
              'region': 1, 'replication_ip': '10.24.0.71', 'replication_port': 6002, 'weight': 180.0, 'zone': 3},
             {'device': 'sdi', 'id': 59, 'ip': '10.24.0.72', 'meta': u'', 'port': 6002,
-             'region': 1, 'replication_ip': '10.24.0.72', 'replication_port': 6002, 'weight': 360.0, 'zone': 2}])
+             'region': 1, 'replication_ip': '10.24.0.72', 'replication_port': 6002, 'weight': 360.0, 'zone': 2}
+            ])
 
         response_mock = Mock()
         response_mock.configure_mock(
             status_code=204,
             headers=CaseInsensitiveDict({'x-account-bytes-used': '368259416'}),
             )
-        poe.requests.head.return_value = response_mock
+        _requests_head.return_value = response_mock
 
         # Assert that _get_account_ring does what we expect.
         self.assertEqual(s._get_account_usage('AUTH_12bb569bf909441b90791482ae6f9ca9'), 368259416)
